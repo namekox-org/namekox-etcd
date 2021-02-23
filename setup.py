72d1f3a803ac94abf37998ba99a7b027d5e5cf0a
李满满 <limanman@corp.netease.com>
2021-02-23 22:10:32 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #! -*- coding: utf-8 -*-
-
+#
 # author: forcemain@163.com
 
+from __future__ import unicode_literals
 
 import os
 
@@ -15,7 +16,7 @@ readme = open(os.path.join(curdir, 'READIT.md'), 'rb', 'utf-8').read()
 
 setup(
     name='namekox-etcd',
-    version='0.0.3',
+    version='0.0.4',
     description='namekox etcd',
     long_description_content_type='text/markdown',
     long_description=readme,
